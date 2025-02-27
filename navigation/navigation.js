3e0e858605573e417fe1b2d1b711d7646d1ffa16
Maxim <maks.shepelev.08@bk.ru>
2025-02-28 01:22:32 +0500

    two screens

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -1,11 +1,13 @@
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Drawing from '../screens/Drawing';
+import Menu from '../screens/menu';
 
 const MainScreen_Stack = createNativeStackNavigator()
 function Main_StackNavigator(){
     return(
       <MainScreen_Stack.Navigator>
+      <MainScreen_Stack.Screen name="Галерея" options={{headerShown: false}} component={Menu} />
       <MainScreen_Stack.Screen name="Рисовалки" options={{headerShown: false}} component={Drawing} />
     </MainScreen_Stack.Navigator>
   )
