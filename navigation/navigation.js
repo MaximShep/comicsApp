9269f4ac1ca0e531b40a6f575f642b3f896355c7
Maxim <maks.shepelev.08@bk.ru>
2025-03-01 00:54:30 +0500

    forArtyom

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -1,56 +1,115 @@
-import { NavigationContainer, getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import Drawing from '../screens/drawing/Drawing';
 import Menu from '../screens/drawing/menu';
 import Profile from '../screens/profile/Profile';
 import { Ionicons } from "@expo/vector-icons";
 
-const Stack = createNativeStackNavigator();
-const BottomTab = createBottomTabNavigator();
 
-function MainStackNavigator() {
+const stack = createNativeStackNavigator()
+const bottomTab = createBottomTabNavigator();
+
+function Main_StackNavigator(){
+    return(
+      <stack.Navigator options={{headerShown: false}}>
+      <stack.Screen name="Галерея" options={{headerShown: false}} component={Menu} />
+      <stack.Screen name="Рисовалки" options={{headerShown: false}} component={Drawing} />
+    </stack.Navigator>
+  )
+}
+
+// function AfterLogin(){
+//     return (
+//         <stack.Navigator screenOptions={{ headerShown: false }}>
+//           <stack.Screen name="qq" component={LoginScreen} />
+//           <stack.Screen name="ww" component={BottomTab_Navigator} />
+//         </stack.Navigator>
+//       );
+// }
+
+
+// function MainStack() {
+//   return (
+//     <stack.Navigator screenOptions={{ headerShown: false }}>
+//       <stack.Screen name="qq" component={MainScreen} />
+//       <stack.Screen name="ww" component={RecipeDetail} />
+//     </stack.Navigator>
+//   );
+// }
+
+// function CreateRecipeStack() {
+//   return (
+//     <stack.Navigator screenOptions={{ headerShown: false }}>
+//       <stack.Screen name="qq" component={CustomRecipes} />
+//       <stack.Screen name="ww" component={CreateRecipe} />
+//     </stack.Navigator>
+//   );
+// }
+
+// function CustomRecipeStack() {
+//   return (
+//     <stack.Navigator screenOptions={{ headerShown: false }}>
+//       <stack.Screen name="CreateRecipe" component={CreateRecipeStack} />
+//       <stack.Screen name="RecipeDetail" component={RecipeDetail_Custom} />
+//     </stack.Navigator>
+//   );
+// }
+
+
+
+// Навигация для экранов авторизации
+function AuthStack() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Галерея" component={Menu} />
-      <Stack.Screen name="Рисовалки" component={Drawing} />
-    </Stack.Navigator>
+    <stack.Navigator screenOptions={{ headerShown: false }}>
+      <stack.Screen name="Login" component={AfterLogin} />
+      <stack.Screen name="Register" component={RegisterScreen} />
+    </stack.Navigator>
   );
 }
 
-function BottomTabNavigator() {
+function BottomTab_Navigator() {
   return (
-    <BottomTab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName;
-          if (route.name === "Главная") iconName = "home";
-          else if (route.name === "Комиксы") iconName = "people-circle-outline";
-          else if (route.name === "Профиль") iconName = "person";
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: "tomato",
-        tabBarInactiveTintColor: "gray",
-        headerShown: false, // Скрываем заголовки для всех экранов BottomTab
-        tabBarStyle: ({ state }) => {
-          const route = state.routes[state.index];
-  const currentRoute = route.state?.routes[route.state.index]?.name || route.name;
-  return currentRoute === 'Рисовалки' ? { display: 'none' } : {};
-        },
-      })}
-    >
-      <BottomTab.Screen name="Главная" component={MainStackNavigator} />
-      <BottomTab.Screen name="Комиксы" component={Menu} />
-      <BottomTab.Screen name="Профиль" component={Profile} />
-    </BottomTab.Navigator>
+      <bottomTab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === "Главная") {
+              iconName = "home";
+            } else if (route.name === "Комиксы") {
+              iconName = "people-circle-outline";
+            } else if (route.name === "Профиль") {
+              iconName = "person";
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarStyle: {
+            display: route.name === "Рисовалки" ? 'none' : 'flex', // Скрываем bottomTab на экране Details
+          },
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        
+            <bottomTab.Screen options={{headerShown: false}}name="Главная" component={Main_StackNavigator} />
+            <bottomTab.Screen options={{headerShown: false}}name="Комиксы" component={Menu} />
+            <bottomTab.Screen name="Профиль" component={Profile} />
+      </bottomTab.Navigator>
   );
 }
 
-export default function AppNavigation() {
-  return (
-    <NavigationContainer>
-      <BottomTabNavigator />
-    </NavigationContainer>
-  );
+
+export default function AppNavigation(){
+    return(
+       
+        <NavigationContainer>
+           <BottomTab_Navigator/>
+        </NavigationContainer>
+       
+    )
 }
+
+
